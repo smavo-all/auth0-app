f9745304cecd6f0213bdfaba666fa14e05b4b233
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-03 23:16:04 -0500

    add rutas al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
